d33ac52713142e332ac312cc90c36e7841b915ff
Tobias Rohde <tobiasr@uw.edu>
2020-02-13 00:03:53 -0800

    added slides and narrative, fixed lines, changed their color,

--- src/d3.js
+++ src/d3.js
@@ -2,15 +2,29 @@ import * as d3 from "d3";
 import * as tf from "@tensorflow/tfjs";
 
 import * as config from "./config";
-import {initSVG, initInputImg, initKernelImg, initOutputImg, initEffects} from "./initSVG";
+
+import {initSVG, initInputImg, initKernelImg, initOutputImg, initEffects, initAnnotations, updateAnnotation} from "./initSVG";
 import {drawInputData, drawKernelData, drawOutputData, drawEffects, removeEffects, grayToFloat, drawOutputDataPoint} from "./updateSVG";
 import {createConv} from "./tensor";
+import {Slide} from "./slide"
 
 // Image data
 export let image = [[]];
 export let resultImg = [[]];
 export let visibleImg = [[]];
 export let kernel = [[]];
+
+export let slide_idx = 0
+let slides = [
+    new Slide("Bird", "demo", "Convolution simply takes two matrics of the same size, multiplies corresponding entries and sums them up. \n \n " +
+                              "Mouse over the left input matrix to convolve small patches with the kernel below! (Or use 'Auto Conv' to do it automatically) \n \n Click 'Next' once " + 
+                              "understand how the math works.", 0, 0),
+    new Slide("Bird", "edge_detection", "Usually convolution is applied to images, where the numbers represent colors. \n \n " +
+        "The kernel is applied to each color channel (r, g, b). \n \n " + 
+        "The kernel below is an edge detection filter. Use it to highlight the edges of the bird. ", 0, 0),       
+    
+
+    new Slide(null, null, "Now try convolving images on your own! Choose an image and filter from above.", 0, 0)]
 
 /**
  * Given a url to an image, displays the image as a matrix of pixels.
@@ -39,7 +53,14 @@ function loadImage(url) {
         for (let i = 0; i < imgData.data.length; i += 4) {
             let x = (i / 4) % canvas.width;
             let y = Math.floor((i / 4) / canvas.width);
-            pixelValues[y][x] = [imgData.data[i], imgData.data[i + 1], imgData.data[i + 2]];
+
+            let color = [imgData.data[i], imgData.data[i + 1], imgData.data[i + 2]];
+
+            if (slide_idx == 0) { // Just do 0's and 1's for this one
+                let gray = (imgData.data[i] + imgData.data[i + 1] + imgData.data[i + 2]) / 3 / 255
+                color[0] = (gray <= 0.7) | 0
+            }
+            pixelValues[y][x] = color;
         }
 
         image = pixelValues;
@@ -59,7 +80,11 @@ function animateConv() {
     d3.select("#auto-conv").text("Stop").on("click", () => { stop_anim = true; });
     d3.select("#filter-selection").attr("disabled", "disabled")
     d3.select("#image-selection").attr("disabled", "disabled")
-    visibleImg = [...Array(resultImg[0].length)].map(() => [...Array(resultImg.length)].map(() => 0));
+    d3.select("#next").attr("disabled", "disabled")
+    d3.select("#prev").attr("disabled", "disabled")
+
+    let default_val = slide_idx == 0 ? 0 : 255;
+    visibleImg = [...Array(config.outputWidth)].map(() => [...Array(config.outputHeight)].map(() => [default_val, default_val, default_val]));
 
     drawInputData(true);
     drawOutputData(true);
@@ -82,6 +107,8 @@ function animateConv() {
             d3.select("#auto-conv").text("Auto Conv").on("click", animateConv);
             d3.select("#filter-selection").attr("disabled", null)
             d3.select("#image-selection").attr("disabled", null)
+            d3.select("#next").attr("disabled", null)
+            d3.select("#prev").attr("disabled", null)
         } else {
             ++pixel;
         }
@@ -92,7 +119,27 @@ function animateConv() {
  * Updates display and data with new filter and image choice.
  */
 function updateData() {
-    switch (d3.select("#filter-selection").node().value) {
+    let kernel_name = null;
+    let img_url = null;
+
+    if (slide_idx == slides.length - 1) {
+        img_url = d3.select("#image-selection").node().value;
+        kernel_name = d3.select("#filter-selection").node().value;
+    }
+    else {
+        let options = Array.apply(null, d3.select("#image-selection").node().options)
+        let opt_idx = options.findIndex((v, i, arr) => v.text == slides[slide_idx].img)
+        img_url = options[opt_idx].value
+
+        kernel_name = slides[slide_idx].kernel
+    }
+
+    switch (kernel_name) {
+        case "demo":
+            kernel = [[0, 2, 0],
+                      [2, 1, 2],
+                      [0, 2, 0]];
+            break
         case "identity":
             kernel = [[0, 0, 0],
                       [0, 1, 0],
@@ -109,9 +156,9 @@ function updateData() {
                       [-1, -2, -1]];
             break;
         case "edge_detection":
-            kernel = [[0,  1, 0],
-                      [1, -4, 1],
-                      [0,  1, 0]];
+            kernel = [[-1,  -1, -1],
+                      [-1, 8, -1],
+                      [-1,  -1, -1]];
             break;
         case "sharpen":
             kernel = [[ 0, -1,  0],
@@ -125,9 +172,7 @@ function updateData() {
             break;
     }
 
-    const img = d3.select("#image-selection");
-    // This is where we get the url for the Image
-    loadImage(img.node().value);
+    loadImage(img_url);
 }
 
 /**
@@ -139,17 +184,63 @@ function refreshData() {
     const r = image.map(row => row.map(v => v[0]));
     const g = image.map(row => row.map(v => v[1]));
     const b = image.map(row => row.map(v => v[2]));
+    console.log(r)
     resultImg = tf.concat([
         convLayer.apply(tf.reshape(tf.tensor(r), [1, image[0].length, image.length, 1])),
         convLayer.apply(tf.reshape(tf.tensor(g), [1, image[0].length, image.length, 1])),
         convLayer.apply(tf.reshape(tf.tensor(b), [1, image[0].length, image.length, 1]))
         ], 3).arraySync()[0];
 
-    visibleImg = [...Array(resultImg[0].length)].map(() => [...Array(resultImg.length)].map(() => 0));
+    let default_val = slide_idx == 0 ? 0 : 255;
+    visibleImg = [...Array(resultImg[0].length)].map(() => [...Array(resultImg.length)].map(() => [default_val, default_val, default_val]));
 
     drawInputData(false);
     drawOutputData(false);
     drawKernelData();
+}
+
+function update_slide() {
+    d3.select("#annotation")
+        .style("visibility", "visible");
+    updateAnnotation(slides[slide_idx].annotation)
+
+    let vis = (slide_idx == slides.length - 1) ? "visible" : "hidden";
+    d3.select("#filter-selection").style("visibility", vis)
+    d3.select("#image-selection").style("visibility", vis)
+}
+
+function prev_slide() {
+    if (slide_idx == 0)
+        return
+
+    --slide_idx;
+
+    if (slide_idx == 0)
+        d3.select("#prev").style("visibility", "hidden");
+    else
+        d3.select("#prev").style("visibility", "visible");
+    
+    d3.select("#next").style("visibility", "visible");
+
+    update_slide()
+    updateData();
+}
+
+function next_slide() {
+    if (slide_idx == slides.length - 1)
+        return
+
+    ++slide_idx;
+
+    if (slide_idx == slides.length - 1)
+        d3.select("#next").style("visibility", "hidden");
+    else
+        d3.select("#next").style("visibility", "visible");
+    
+    d3.select("#prev").style("visibility", "visible");
+
+    update_slide()
+    updateData();
 }
 
 /**
@@ -161,13 +252,20 @@ function main() {
     initInputImg();
     initOutputImg();
     initEffects();
-
+    initAnnotations();
+    update_slide();
     updateData();    
 }
 
+d3.select("#filter-selection").style("visibility", "hidden")
+d3.select("#image-selection").style("visibility", "hidden")
 d3.select("#filter-selection").on("change", updateData);
 d3.select("#image-selection").on("change", updateData);
 
 d3.select("#auto-conv").on("click", animateConv);
+
+d3.select("#prev").style("visibility", "hidden");
+d3.select("#prev").on("click", prev_slide)
+d3.select("#next").on("click", next_slide)
 
 window.onload = main;
